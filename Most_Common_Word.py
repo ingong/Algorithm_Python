f3dffe0d430a51b5e67dd1db2b41589d7214d1b4
leeinsong <insong1216@gmail.com>
2021-03-18 14:12:59 +0900

    Add: Most Common Word

---
+++ Most_Common_Word.py
@@ -0,0 +1,16 @@
+from typing import List
+import collections
+import re
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+                 .lower().split()
+                        if word not in banned]
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
+
+solution = Solution()
+result: str = solution.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"])
+print(result)
+
