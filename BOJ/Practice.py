d967ebf86f8339876f9e4ec08a6ad819912f19c8
leeinsong <insong1216@gmail.com>
2021-06-08 16:55:46 +0900

    Programmers 210608 화

--- BOJ/Practice.py
+++ BOJ/Practice.py
@@ -1,8 +1,18 @@
-a = "a123"
+import sys
+n, m = 3, 3
+c = [False for _ in range(n+1)]
+a = [0 for _ in range(m)]
 
-for i in a:
-    print(i)
 
+def go(index, n):
+    if index == n:
+        print(a)
+    for i in range(1, n+1):
+        if c[i]:
+            continue
+        c[i] = True
+        a[index] = i
+        go(index+1, n)
+        c[i]= False
 
-for i in range(len(a)):
-    print(a[i].isdigit())
+go(0, n)
