cb2e6abbb46ca205f6d801dfef71e25e48ab4d61
leeinsong <insong1216@gmail.com>
2021-06-29 02:14:22 +0900

    add: 15721 뻔데기

---
+++ BoostCamp/210629/BOJ[15721]:번데기.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+people = int(input())
+number = int(input())
+word = int(input())
+
+n = 2
+count = 0
+start = ['뻔', '데기', '뻔', '데기']
+result = []
+
+while True:
+    repeat = ["뻔"] * n + ["데기"] * n
+    result += (start + repeat)
+    if len(result) > 10000:
+        break
+    n += 1
+
+if word == 0:
+    keyword = "뻔"
+else:
+    keyword = "데기"
+
+for i, val in enumerate(result):
+    if val == keyword:
+        count += 1
+    if count == number:
+        print((i % people))
+        break
+
