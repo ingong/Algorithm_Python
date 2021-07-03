244c047e308929e4b99c331b07ae9b0798cb492f
leeinsong <insong1216@gmail.com>
2021-07-03 12:00:26 +0900

    다익스트라 알고리즘

--- BoostCamp/210703/Floodfill.py
+++ BoostCamp/210703/Floodfill.py
@@ -1,10 +1,12 @@
 from collections import deque
 
-
+# visitable 로 가능한지 체크하는거 좋다
 def visitable(n, m, y, x):
     return 0 <= y < n and 0 <= x < m
 
 
+# 2차원 배열일 경우, 특히 좌표의 특성을 가질 때는 이런식으로 해주는거 굿인듯
+# visited
 def solution(n, m, image):
     visited, bfs_nodes = set(), deque([])
     deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
